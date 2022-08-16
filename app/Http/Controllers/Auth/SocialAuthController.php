d006b6084fdc0bc6e107bbe794b3759bdaca5dfe
Achref taleb <achraf.taleb.webify@gmail.com>
2022-08-16 11:42:18 +0000

    Update app/Http/Controllers/Auth/SocialAuthController.php

--- app/Http/Controllers/Auth/SocialAuthController.php
+++ app/Http/Controllers/Auth/SocialAuthController.php
@@ -120,7 +120,7 @@ class SocialAuthController extends Controller
             }
         } catch (\Exception $exception) {
             if (env('APP_DEBUG')) {
-                $res->fail($exception->message);
+                $res->fail($exception->getMessage());
             }
             $res->fail('erreur serveur 500');
             return new JsonResponse($res, $res->code);
