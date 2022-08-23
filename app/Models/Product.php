4e4e446e0bb9fd1c1ceaa00031d7c491497e4ea7
achref <achraf.taleb.webify@gmail.com>
2022-08-23 13:28:09 +0200

    fix menu propagation issue

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -8,7 +8,8 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Product extends Model
 {
-    use HasFactory,SoftDeletes;
+    use HasFactory, SoftDeletes;
+
     protected $table = 'products';
     protected $fillable = [
         'name',
@@ -40,53 +41,58 @@ class Product extends Model
 //                                   'option_id',
 //                                   'supplier_id');
 //    }
-   public function options() {
-    return $this->belongsToMany(
-                              'App\Models\Option',
-                              'supplier_product_option',
-                              'product_id',
-                              'option_id'
-                              );
-}
-public function menu() {
-    return $this->belongsToMany(
-                              'App\Models\Menu',
-                              'supplier_menu_product',
-                              'product_id',
-                              'menu_id'
-                              );
-}
-   public function suppliers()
-   {
-       return $this->belongsToMany(Supplier::class)->withPivot('price');
-   }
-   public function panier()
-   {
-       return $this->belongsToMany(Panier::class)->withPivot('quantity');
-   }
-   public function typeproduct()
-   {
-       return $this->belongsToMany(TypeProduct::class);
-   }
-   public function tag()
-   {
-       return $this->belongsToMany(Tag::class);
-   }
-   public function files()
-   {
-       return $this->belongsToMany(File::class);
-   }
-   public function product_hours()
-   {
-       return $this->hasOne(Product_hours::class);
-   }
-   public function discounds()
-   {
-       return $this->hasMany(Discount::class);
-   }
+    public function options()
+    {
+        return $this->belongsToMany(
+            'App\Models\Option',
+            'supplier_product_option',
+            'product_id',
+            'option_id'
+        );
+    }
 
-   public function supplier_price($supplier)
-   {
-     return  $this->suppliers()->wherePivot('supplier_id',$supplier)->withPivot('price')->first()->pivot->price;
-   }
+    public function menu()
+    {
+        return $this->belongsToMany(Menu::class);
+    }
+
+    public function suppliers()
+    {
+        return $this->belongsToMany(Supplier::class)->withPivot('price');
+    }
+
+    public function panier()
+    {
+        return $this->belongsToMany(Panier::class)->withPivot('quantity');
+    }
+
+    public function typeproduct()
+    {
+        return $this->belongsToMany(TypeProduct::class);
+    }
+
+    public function tag()
+    {
+        return $this->belongsToMany(Tag::class);
+    }
+
+    public function files()
+    {
+        return $this->belongsToMany(File::class);
+    }
+
+    public function product_hours()
+    {
+        return $this->hasOne(Product_hours::class);
+    }
+
+    public function discounds()
+    {
+        return $this->hasMany(Discount::class);
+    }
+
+    public function supplier_price($supplier)
+    {
+        return $this->suppliers()->wherePivot('supplier_id', $supplier)->withPivot('price')->first()->pivot->price;
+    }
 }
