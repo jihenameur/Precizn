adf7fbaa267d4a391f3d8cd5d58d90aee3462a22
achref <achraf.taleb.webify@gmail.com>
2022-09-02 00:20:42 +0200

    finish dispatcher endpoints & events

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -16,7 +16,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
          $schedule->command('commands:assign')->everyMinute();
-         $schedule->command('simulation:commands')->everyMinute();
+         $schedule->command('simulation:commands')->everyFiveMinutes();
 
     }
 
