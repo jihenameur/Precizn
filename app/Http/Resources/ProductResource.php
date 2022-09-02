0aeae681904bfdc94de2db6e1328e64432c75be0
Mehdi Ben Jouida <mehdi.benjouida.webify@gmail.com>
2022-09-02 17:05:27 +0200

    fixproducthour

--- app/Http/Resources/ProductResource.php
+++ app/Http/Resources/ProductResource.php
@@ -9,30 +9,30 @@ class ProductResource extends JsonResource
     /**
      * Transform the resource into an array.
      *
-     * @param  \Illuminate\Http\Request  $request
+     * @param \Illuminate\Http\Request $request
      * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
      */
     public function toArray($request)
     {
         return [
             'id' => $this->id,
-            'name' =>$this->name,
-            'description' =>$this->description,
-            'default_price' =>$this->default_price,
-            'private' =>$this->private,
-            'min_period_time' =>$this->min_period_time,
-            'max_period_time' =>$this->max_period_time,
-            'available' =>$this->available,
-            'unit_type' =>$this->unit_type,
-            'unit_limit' =>$this->unit_limit,
-            'weight' =>$this->weight,
-            'dimension' =>$this->dimension,
+            'name' => $this->name,
+            'description' => $this->description,
+            'default_price' => $this->default_price,
+            'private' => $this->private,
+            'min_period_time' => $this->min_period_time,
+            'max_period_time' => $this->max_period_time,
+            'available' => $this->available,
+            'unit_type' => $this->unit_type,
+            'unit_limit' => $this->unit_limit,
+            'weight' => $this->weight,
+            'dimension' => $this->dimension,
             'options' => ProductOptionsResource::collection($this->options),
             'image' => FileResource::collection($this->files),
             'type_product' => TypeProductResource::collection($this->typeproduct),
             'tags' => TagResource::collection($this->tag),
             'menu' => MenuResource::collection($this->menu),
-
+            'hours' => new ProductHourResource($this->product_hours),
             'supplier' => SupplierResource::collection($this->suppliers)
         ];
     }
